2d40a042472c0243d1606f3b50a29bf4b0d7abdc
NajamulNisa <najamulnisa282@gmail.com>
2024-04-30 12:10:42 +0530

    new changes

--- src/components/DataUploader.js
+++ src/components/DataUploader.js
@@ -1,42 +1,41 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function DataUploader({ onDataUpload }) {
+function DataUploader() {
   const [file, setFile] = useState(null);
+  const [uploadedData, setUploadedData] = useState(null);
 
-  const handleFileChange = (event) => {
+  const handleFileUpload = (event) => {
     setFile(event.target.files[0]);
   };
 
   const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append('file', file);
+
     try {
-      if (!file) {
-        console.error('File is required.');
-        return;
-      }
-
-      const formData = new FormData();
-      formData.append('file', file); // Append the file with the field name 'file'
-
-      // Verify the FormData contents (optional)
-      console.log([...formData.entries()]);
-
-      // Make the request to the server
-      const response = await axios.post('https://cors-anywhere.herokuapp.com/https://fastapi-x21t.onrender.com/upload/', formData);
-
-      // Handle the response
-      onDataUpload(response.data);
+      const response = await axios.post('https://cors-anywhere.herokuapp.com/https://fastapi-x21t.onrender.com/upload/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      setUploadedData(response.data);
     } catch (error) {
       console.error('Error uploading file:', error);
     }
   };
 
   return (
-    <div className="data-uploader">
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload CSV</button>
+    <div>
+      <input type="file" onChange={handleFileUpload} />
+      <button onClick={handleUpload}>Upload</button>
+      {uploadedData && (
+        <div>
+          <h2>Uploaded Data:</h2>
+          <pre>{JSON.stringify(uploadedData, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
-}
-
+};
 export default DataUploader;
